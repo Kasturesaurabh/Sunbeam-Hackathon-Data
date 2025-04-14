5928e29ad7121d6e0f6783fbb62b76d3246b6c27
Saurabh <saurabhkasture04@gmial.com>
2025-04-14 19:05:41 +0530

    ADD

--- MoviesManagement/src/Main.java
+++ MoviesManagement/src/Main.java
@@ -1,9 +1,40 @@
 
+import java.util.*;
+import java.util.Scanner;
+
 public class Main {
+	Scanner sc = new Scanner(System.in);
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Hello World");
-
-	}
 }
+
+public void showMainMenu() {
+	System.out.println("\n----MAIN MENU----");
+	System.out.println("1. Sign Up");
+	System.out.println("2. Sign In");
+	System.out.println("3. Exit");
+	System.out.println("Choose an Option: ");
+	int ch = sc.nextInt();
+	sc.nextLine();
+	
+	switch (ch) {
+	
+	case 1:
+        SignUp();
+        break;
+    case 2:
+        SignIn();
+        break;
+    case 3:
+        System.out.println("Exiting..!");
+        System.exit(0);
+        break;
+    default:
+        System.out.println("Invalid option. please Try again.");
+}
+}
+public void showSignedInMenu() {
+	
+
+}
+
+
